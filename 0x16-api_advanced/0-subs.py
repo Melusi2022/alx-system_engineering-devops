82093bfd6e00bbc2365e0d6d620276c7492921da
Melusi2022 <melusimedia@gmail.com>
2024-04-09 07:35:30 -0700

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-        """queries the Reddit API"""
-            headers = {
-                            "User-Agent": "0x16. API_advanced-e_kiminza"
-                                    }
-                url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-                    response = requests.get(url, headers=headers, allow_redirects=False)
-                        if response.status_code != 200:
-                                    return 0
-                                    sub = response.json().get("data").get("subscribers")
-                                        return sub
+    """queries the Reddit API"""
+    headers = {
+        "User-Agent": "0x16. API_advanced-e_kiminza"
+        }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    sub = response.json().get("data").get("subscribers")
+    return sub
